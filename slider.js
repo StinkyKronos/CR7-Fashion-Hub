0c8d7e5bba12ce779efd43b32150902892ff52f5
StinkyKronos <hemuhemu85672@gmail.com>
2022-02-04 20:51:40 +0530

    Tweaked some setting, Improved resoponsiveness

--- slider.js
+++ slider.js
@@ -2,16 +2,25 @@ const slides = Array.from(document.querySelectorAll(".slide"));
 const slider = document.querySelector('.slider');
 
 
-
 function automaticSlider() {
     const activeSlide = document.querySelector('.slide.active');
-    
+    const nextSlide = activeSlide.nextElementSibling;
+    const prevSlide = activeSlide.previousElementSibling;
+    const nextSlideIndex = slides.indexOf(nextSlide);
+
     activeSlide.style.transform = 'translateX(-100%)';
     activeSlide.classList.remove('active');
-    activeSlide.addEventListener('transitionend', function () {
-        activeSlide.classList.remove('active');
+
+    if (nextSlideIndex == 0) { 
+        slides[1].style = "";
     }
-    );
+    if (nextSlideIndex == 1) { 
+        slides[2].style = "";
+    }
+    if (nextSlideIndex == 2) { 
+        slides[0].style = "";
+    }
+    
     if (activeSlide.nextElementSibling) {
         activeSlide.nextElementSibling.classList.add('active');
         activeSlide.nextElementSibling.style.transform = 'translateX(0)';
@@ -23,5 +32,12 @@ function automaticSlider() {
     
 setInterval(automaticSlider, 3000);
 
+function changeSize(){
+    const sliderHeight = document.getElementById("image").scrollHeight;
+
+    slider.style.height = sliderHeight + "px";
+}
+
+setInterval(changeSize, 500);
 
 
